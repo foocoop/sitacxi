cf5fe23da86ba729c1ad7b3a1570ece3c5f86113
furenku <furenku@debianT410.foocoop.mx>
2013-08-14 11:36:20 -0500

    arreglar visualizacion lecturas

--- single-lectura.php
+++ single-lectura.php
@@ -1,54 +1,56 @@
 <?php
 get_header();
+
+while(have_posts()){
+  the_post();
+  $contenido = get_the_content();
+}
+
+$lecturasPagID = 17;
+$submenu = "";
+$pgs = get_pages( array('child_of'=>$lecturasPagID ) );
+
+foreach ( $pgs as $p ) {
+  $ttl = foo_filter( $p->post_title, 'title');;
+  $url = get_permalink( $p->ID );
+  $submenu .= foo_li("","",$ttl,$url);
+}
+
+$submenu = foo_div("submenu", "", $submenu);
+
+
+$query = new WP_Query(array( 'post_type'=>'lectura','posts_per_page'=>-1 ) );
+if( $query->have_posts() ) {
+  while ( $query->have_posts() ) {
+    $query->the_post();
+    $titulo = get_the_title();
+    $link = get_permalink($post->ID);
+    $autor = get_post_meta($post->ID,'autor');
+    $autor = $autor[0];
+    
+    $lecturas .= foo_li("","lectura",
+                        foo_div("","autor", $autor ) .
+                                foo_div("","titulo", $titulo ),
+                        $link);
+    
+  }
+}
+
+
+echo $submenu;
+
+echo foo_div("lecturas","",$lecturas);
+
+
+echo foo_div("","contenido", foo_filter( $contenido, 'content' )); 
+
 ?>
 
-<div class="large-9 columns" role="main">
-
-  <?php
-
-  $submenu = "";
-  $lecturas = new WP_Query(array( 'post_type'=>'lectura','posts_per_page'=>-1 ) );
-  if( $lecturas->have_posts() ) {
-    while ( $lecturas->have_posts() ) {
-      $lecturas->the_post();
-      $ttl = foo_filter( $post->post_title, 'title');;
-      $url = get_permalink( $post->ID );
-      $submenu .= foo_li("","",$ttl,$url);
-
-
-      echo foo_div("submenu","", foo_ul("","large-block-grid-3",$submenu) );
-    }
-  }
-  wp_reset_query();
-
-  if(have_posts()) {
-    while(have_posts()) {
-      the_post();
-
-  ?>
-  
-
-  <article id="lectura-<?php the_ID(); ?>" <?php post_class(); ?>>
-    
-    <header>
-      <h2><?php the_title(); ?></h2>
-    </header>
-    
-    <?php echo foo_div("","contenido", foo_filter( get_the_content(), 'content' )); ?>
-
-  </article>
-  
-  <?php
-    }
-  }
-  ?>
-
-</div>
 
 <script type="text/javascript">
 
  jQuery(document).ready(function($) {
-   var link = $('article .contenido a');
+   var link = $('.contenido a');
    var reader = $("#reader #reader");
    var newlink = link.detach();
    reader.html( newlink );
