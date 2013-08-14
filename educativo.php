2b8c7b3e969b5edae7cc91f5d2a6b2cd8eeddd03
furenku <furenku@debianT410.foocoop.mx>
2013-08-13 23:16:11 -0500

    corregir errores

--- educativo.php
+++ educativo.php
@@ -5,37 +5,35 @@ Template Name: Educativo
 
 get_header(); ?>
 
-<!-- Main Content -->
+
 <div class="large-9 columns" role="main">
 
   <?php
-if(!empty($post->post_parent)) {
-  
-};
+
   if ( have_posts() ) {
     while ( have_posts() ) {
       the_post();
 
       if( !empty($post->post_parent ) ) {
-  $parent = $post -> post_parent;
-  
+        $parent = $post -> post_parent;
+        
         $pgs = get_pages( array('child_of'=>$parent ) );
       }
       else {
         $pgs = get_pages( array('child_of'=>$post->ID ) );
-  }
+      }
 
-  
-  $submenu = "";
+      
+      $submenu = "";
       foreach ( $pgs as $p ) {
         $ttl = foo_filter( $p->post_title, 'title');;
         $url = get_permalink( $p->ID );
         $submenu .= foo_li("","",$ttl,$url);
       }
 
-  echo foo_div("submenu","",$submenu);
+      echo foo_div("submenu","",$submenu);
+      
+    }
+  }
 
-
-
-
-  get_footer(); ?>
+get_footer(); ?>
