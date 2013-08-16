c8172614a688a07510e6f08526f3bb767c2b57c1
furenku <furenku@debianT410.foocoop.mx>
2013-08-16 11:39:11 -0500

    orden alfabetico

--- invitados.php
+++ invitados.php
@@ -9,16 +9,25 @@ get_header(); ?>
 
   <?php
   $submenu = "";
+  $preOrden = array();
   $invitados = new WP_Query(array( 'post_type'=>'invitado','posts_per_page'=>-1 ) );
   if( $invitados->have_posts() ) {
     while ( $invitados->have_posts() ) {
       $invitados->the_post();
       $ttl = foo_filter( $post->post_title, 'title');;
       $url = get_permalink( $post->ID );
-      $submenu .= foo_li("","",$ttl,$url);
+      $nombre = explode( " ", $ttl );
+      $apellido = $nombre[1];
+      $preOrden[$apellido] = foo_li("","",$ttl,$url);     
     }
   }
 
+  ksort($preOrden);
+  
+  foreach($preOrden as $pO => $li ) {
+    $submenu .= $li;
+    
+  }
   $submenu = foo_div("submenu", "", foo_ul("","large-block-grid-3", $submenu));
   
   echo $submenu;
